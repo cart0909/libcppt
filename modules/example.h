adb1f2a1eda00d21b2288f41fc8a386be2ef05e2
Yi-Ren <yirenliu0909@gmail.com>
2018-08-14 01:19:50 +0800

    add project setting

---
+++ modules/example.h
@@ -0,0 +1,8 @@
+#pragma once
+
+double add_numbers(const double f1, const double f2);
+
+double subtract_numbers(const double f1, const double f2);
+
+double multiply_numbers(const double f1, const double f2);
+
