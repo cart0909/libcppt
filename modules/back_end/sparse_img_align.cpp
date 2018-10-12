5a7b84ea2fd7502ea0673fcc84bf3bc1672f0918
carl_liu <yirenliu0909@gmail.com>
2018-10-12 18:29:40 +0800

    add more sparse image alignment.

--- modules/back_end/sparse_img_align.cpp
+++ modules/back_end/sparse_img_align.cpp
@@ -1,1 +1,62 @@
 #include "sparse_img_align.h"
+
+SparseImgAlign::SparseImgAlign() {
+
+}
+
+SparseImgAlign::~SparseImgAlign() {
+
+}
+
+void SparseImgAlign::Run() {
+    int num_pts = mvRefPts.size();
+    mRefPatch.create(num_pts, mPatchArea, CV_32F);
+    mJpatch_pt.resize(Eigen::NoChange, num_pts * mPatchArea);
+    mvIsVisible.resize(num_pts, false);
+
+    for(mCurLevel = mMaxLevel; mCurLevel >= mMinLevel; --mCurLevel) {
+        Optimize();
+    }
+}
+
+bool SparseImgAlign::Solve() {
+    mdelx = mH.ldlt().solve(mb);
+    if(std::isnan((double)mdelx(0)))
+        return false;
+    return true;
+}
+
+void SparseImgAlign::Update() {
+    mTcr = mTcr * Sophus::SE3d::exp(-mdelx);
+}
+
+void SparseImgAlign::Optimize() {
+
+}
+
+void SparseImgAlign::ComputeReferencePatchAndJacobian() {
+    const int border = mPatchHalfSize + 1;
+    const cv::Mat& ref_img = mvRefImgPyr[mCurLevel];
+    const int stride = ref_img.cols;
+    const float scale = 1.0f / (1 << mCurLevel);
+    const Eigen::Vector3d ref_pos = mTrw.inverse().translation();
+    const double& fx = mpCamera->fx;
+    size_t feature_counter = 0;
+
+    Eigen::Matrix3d Rrw = mTrw.rotationMatrix();
+    Eigen::Vector3d trw = mTrw.translation();
+    for(int i = 0, n = mvIsVisible.size(); i < n; ++i) {
+        const float u_ref = mvRefPts[i].x() * scale;
+        const float v_ref = mvRefPts[i].y() * scale;
+        const float u_ref_i = std::floor(u_ref);
+        const float v_ref_i = std::floor(v_ref);
+        if(u_ref_i - border < 0 || v_ref_i - border < 0 ||
+           u_ref_i + border >= ref_img.cols || v_ref_i + border >= ref_img.rows)
+            continue;
+        mvIsVisible[i] = true;
+
+        Eigen::Vector3d x3Dc = Rrw * mvRefMps[i] + trw;
+        Eigen::Matrix<double, 2, 3> Jproj;
+//        mpCamera->Project()
+    }
+}
--- modules/back_end/sparse_img_align.h
+++ modules/back_end/sparse_img_align.h
@@ -3,6 +3,7 @@
 #include <Eigen/Dense>
 #include <opencv2/opencv.hpp>
 #include <sophus/se3.hpp>
+#include "camera_model/pinhole_camera.h"
 
 // From SVO SparseImgAlign
 class SparseImgAlign {
@@ -11,15 +12,33 @@ public:
     SparseImgAlign();
     ~SparseImgAlign();
 
-    std::vector<cv::Mat> mRefImgPyr;
-    std::vector<cv::Point2f> mRefPts;
-    std::vector<cv::Mat> mCurImgPyr;
-    Sophus::SE3d mTcf;
+    void Run();
+    bool Solve();
+    void Update();
+    void Optimize();
+    void ComputeReferencePatchAndJacobian();
+
+    std::vector<cv::Mat> mvRefImgPyr;
+    std::vector<cv::Mat> mvCurImgPyr;
+    std::vector<Eigen::Vector2d> mvRefPts;
+    std::vector<Eigen::Vector3d> mvRefMps; // x3Dw
+    Sophus::SE3d mTrw;
+    Sophus::SE3d mTcr;
 
     int mMaxLevel; // coarsest pyramid level for the alignment.
     int mMinLevel; // finest pyramid level for the alignment.
+    int mCurLevel;
 
     int mPatchHalfSize; // 2
     int mPatchSize;     // 4
     int mPatchArea;     // 16
+
+    int mMaxIter; // 10?
+
+    cv::Mat mRefPatch;
+    Eigen::Matrix<double, 6, Eigen::Dynamic> mJpatch_pt;
+    std::vector<bool> mvIsVisible;
+    Eigen::Matrix<double, 6, 6> mH;
+    Eigen::Matrix<double, 6, 1> mb, mdelx;
+    PinholeCameraPtr mpCamera;
 };
