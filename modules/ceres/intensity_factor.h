43797d2969b75fb3cae6866f36aacade1e657dfe
carl_liu <yirenliu0909@gmail.com>
2018-11-14 17:37:16 +0800

    finish sparse image alignment (no debug)

---
+++ modules/ceres/intensity_factor.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "ceres/local_parameterization_se3.h"
+#include <opencv2/opencv.hpp>
+#include <camera_model/pinhole_camera.h>
+
+class IntensityFactor : public ceres::SizedCostFunction<16, 7>
+{
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    IntensityFactor(PinholeCameraConstPtr camera, const cv::Mat& img_ref,
+                    const cv::Mat& img_cur, const Eigen::Vector3d& _x3Dr,
+                    const Eigen::Vector2d& uv_r,
+                    int level);
+
+    ~IntensityFactor() {}
+    virtual bool Evaluate(double const* const* parameters_raw,
+                          double* residuals_raw, double** jacobian_raw) const;
+
+    static const int patch_halfsize;
+    static const int patch_size;
+    static const int patch_area;
+
+    int mLevel;
+    Eigen::Vector3d x3Dr;
+    Eigen::Vector2d uv_r;
+    cv::Mat mImgRef, mImgCur;
+    PinholeCameraConstPtr mpCamera;
+    Eigen::Matrix<double, 16, 1> mRefPatch;
+    Eigen::Matrix<double, 16, 6, Eigen::RowMajor> mJacobian;
+};
