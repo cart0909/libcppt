1a5a076e0962e32249f2378dc436c331315547ad
carl_liu <yirenliu0909@gmail.com>
2018-11-22 12:07:04 +0800

    add mappoint class.

--- modules/basic_datatype/util_datatype.h
+++ modules/basic_datatype/util_datatype.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <memory>
 #include <vector>
 #include <Eigen/Dense>
 #include <opencv2/opencv.hpp>
@@ -8,3 +9,8 @@ using VecVector2d = std::vector<Eigen::Vector2d, Eigen::aligned_allocator<Eigen:
 using VecVector3d = std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d>>;
 using VecVector2f = std::vector<Eigen::Vector2f, Eigen::aligned_allocator<Eigen::Vector2f>>;
 using VecVector3f = std::vector<Eigen::Vector3f, Eigen::aligned_allocator<Eigen::Vector3f>>;
+
+#define SMART_PTR(NAME) \
+using NAME##Ptr = std::shared_ptr<NAME>; \
+using NAME##ConstPtr = std::shared_ptr<const NAME>; \
+using NAME##WPtr = std::weak_ptr<NAME>;
