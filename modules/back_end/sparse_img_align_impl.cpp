0f0bd5ae1fd4018e4f7d36666d56bf3aaec1ed1d
carl_liu <yirenliu0909@gmail.com>
2018-12-03 11:54:22 +0800

    remove the original sparse img align files.

--- modules/back_end/sparse_img_align_impl.cpp
+++
@@ -1,5 +0,0 @@
-#include "sparse_img_align_impl.h"
-
-const int SparseImgAlignImpl::patch_halfsize = 2;
-const int SparseImgAlignImpl::patch_size = 2 * SparseImgAlignImpl::patch_halfsize;
-const int SparseImgAlignImpl::patch_area = SparseImgAlignImpl::patch_size * SparseImgAlignImpl::patch_size ;
