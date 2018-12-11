f1fd27aed7d9061e84474934e57179f4c180a64b
carl_liu <yirenliu0909@gmail.com>
2018-12-11 18:50:26 +0800

    finished the first version of marginalization factor.

--- modules/ceres/marginalization_factor.cpp
+++ modules/ceres/marginalization_factor.cpp
@@ -1,5 +1,19 @@
 #include "marginalization_factor.h"
-#include <sophus/se3.hpp>
+
+void ParameterBlockInfo::linearOMinus(const double* x_raw, const double* x0_raw, double* delx_raw) const {
+    Eigen::Map<const Eigen::VectorXd> x(x_raw, size), x0(x0_raw, size);
+    Eigen::Map<Eigen::VectorXd> delx(delx_raw, size);
+    delx = x - x0;
+}
+
+void SE3BlockInfo::linearOMinus(const double* x_raw, const double* x0_raw, double* delx_raw) const {
+    // example, Twc, Twc0
+    // Twc = Twc0 * delT
+    // delT = Tc0c = Twc0.inv() * Twc
+    Eigen::Map<const Sophus::SE3d> T(x_raw), T0(x0_raw);
+    Eigen::Map<Sophus::SE3d> delT(delx_raw);
+    delT = T0.inverse() * T;
+}
 
 void ResidualBlockInfo::Evaluate() {
     residuals.resize(cost_function->num_residuals());
@@ -48,6 +62,140 @@ void ResidualBlockInfo::Evaluate() {
 
 void MarginalizationInfo::AddResidualBlockInfo(ResidualBlockInfoPtr residual_block_info) {
     factors.emplace_back(residual_block_info);
+
+    auto& parameter_blocks = residual_block_info->parameter_blocks;
+    auto& parameter_block_info = residual_block_info->parameter_block_info;
+    auto parameter_block_sizes = residual_block_info->cost_function->parameter_block_sizes();
+
+    for(int i = 0, n = parameter_blocks.size(); i < n; ++i) {
+        double* addr = parameter_blocks[i];
+        if(parameter_block.find(addr) == parameter_block.end()) {
+            parameter_block[addr] = parameter_block_info[i];
+        }
+    }
+
+    for(auto& idx : residual_block_info->drop_set) {
+        double* addr = parameter_blocks[idx];
+        auto block_it = parameter_block.find(addr);
+        assert(block_it != parameter_block.end());
+        if(block_it->second->idx != 0)
+            block_it->second->idx = 0;
+    }
+}
+
+void MarginalizationInfo::PreMarginalize() {
+    for(auto& it : factors) {
+        it->Evaluate();
+
+        std::vector<int> block_sizes = it->cost_function->parameter_block_sizes();
+        for(int i = 0, n = block_sizes.size(); i < n; ++i) {
+            double* addr = it->parameter_blocks[i];
+            int size = block_sizes[i];
+            auto block_it = parameter_block.find(addr);
+            assert(block_it != parameter_block.end());
+            if(!block_it->second->data) {
+                block_it->second->data = std::unique_ptr<double[]>(new double[size]);
+                std::memcpy(block_it->second->data.get(),
+                            it->parameter_blocks[i], sizeof(double) * size);
+            }
+        }
+    }
+}
+
+void MarginalizationInfo::Marginalize() {
+    m = 0; // marginal size
+    n = 0; // non marginal size
+    for(auto& it : parameter_block) {
+        auto& block = it.second;
+        if(block->idx == 0) { // marginal
+            block->idx = m;
+            m += block->local_size;
+        }
+    }
+
+    for(auto& it : parameter_block) {
+        auto& block = it.second;
+        if(block->idx == -1) { // non marginal
+            block->idx = m + n;
+            n += block->local_size;
+        }
+    }
+    int total_size = m + n;
+
+    /*
+     * A = [Amm Amr   b = [bmm
+     *      Arm Arr]       brr]
+     */
+
+    Eigen::MatrixXd A(total_size, total_size);
+    Eigen::VectorXd b(total_size);
+    A.setZero();
+    b.setZero();
+
+    // TODO fill the matrix A and vector b using multi-thread
+    for(auto& factor : factors) {
+        for(int i = 0; i < factor->parameter_blocks.size(); ++i) {
+            int idx_i = factor->parameter_block_info[i]->idx;
+            int local_size_i = factor->parameter_block_info[i]->local_size;
+            Eigen::MatrixXd jacobian_i = factor->jacobians[i].leftCols(local_size_i);
+
+            // fill bi
+            b.segment(idx_i, local_size_i) = jacobian_i.transpose() * factor->residuals;
+
+            // fill Aij
+            for(int j = i; j < factor->parameter_blocks.size(); ++j) {
+                int idx_j = factor->parameter_block_info[j]->idx;
+                int local_size_j = factor->parameter_block_info[j]->local_size;
+                Eigen::MatrixXd jacobian_j = factor->jacobians[j].leftCols(local_size_j);
+                A.block(idx_i, idx_j, local_size_i, local_size_j) += jacobian_i.transpose() * jacobian_j;
+                if(i == j)
+                    A.block(idx_j, idx_i, local_size_j, local_size_i) =
+                            A.block(idx_i, idx_j, local_size_i, local_size_j).transpose();
+            }
+        }
+    }
+
+    /*
+     * Solve Ax = b and Amn = Anm^t
+     *
+     *      [Amm Amn [xm  = [bm
+     *       Anm Ann] xn]    bn]
+     *
+     * Using Schur complement
+     *      [ Amm                   Amn  [xm   = [                  bm
+     *          0   Ann-Anm*Amm_inv*Amn]  xn]     bn - Anm*Amm_inv*Amn]
+     *    =>(Ann-Anm*Amm_inv*Amn)xn = bn - Anm*Amm_inv*bm
+     */
+    Eigen::MatrixXd Amm = 0.5 * (A.block(0, 0, m, m) + A.block(0, 0, m, m).transpose()); // ensure the Amm is symmetry
+    // https://eigen.tuxfamily.org/dox/classEigen_1_1SelfAdjointEigenSolver.html
+    Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> saes(Amm); // Amm = VDV^t
+    // Amm_inv = VD^-1V^t
+    Eigen::VectorXd D_inv = Eigen::VectorXd((saes.eigenvalues().array() > eps).select(saes.eigenvalues().array().inverse(), 0));
+    Eigen::MatrixXd Amm_inv = saes.eigenvectors() * D_inv.asDiagonal() * saes.eigenvectors().inverse();
+
+    Eigen::MatrixXd Ann = A.block(m, m, n, n);
+    Eigen::MatrixXd Anm = A.block(m, 0, n, m);
+    Eigen::MatrixXd Amn = A.block(0, m, m, n);
+    Eigen::VectorXd bm = b.segment(0, m);
+    Eigen::VectorXd bn = b.segment(m, n);
+
+    A = Ann - Anm * Amm_inv * Amn;
+    b = bn - Anm * Amm_inv * bm;
+
+    // Ax = b
+    // J^tJ x = J^te
+    // try to find J
+    // decomposition A = VSV^t = J^tJ
+    // then J = sqrt(D)*V^t
+    //      e = (sqrt(D))^-1 * V^t * b
+    saes = Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd>(A);
+    Eigen::VectorXd D = (saes.eigenvalues().array() > eps).select(saes.eigenvalues().array(), 0);
+    D_inv = (saes.eigenvalues().array() > eps).select(saes.eigenvalues().array().inverse(), 0);
+    Eigen::VectorXd D_sqrt = D.cwiseSqrt();
+    Eigen::VectorXd D_inv_sqrt = D_inv.cwiseSqrt();
+
+    linearized_jacobian = D_sqrt.asDiagonal() * saes.eigenvectors().transpose();
+    linearized_residuals = D_inv_sqrt.asDiagonal() * saes.eigenvectors().transpose() * b;
 }
 
 bool MarginalizationFactor::Evaluate(double const *const *parameters_raw,
@@ -71,27 +219,7 @@ bool MarginalizationFactor::Evaluate(double const *const *parameters_raw,
     for(int i = 0; i < static_cast<int>(keep_block.size()); ++i) {
         int size = keep_block[i]->size; // global size
         int idx = keep_block[i]->idx - m;
-        ResidualBlockInfo::VertexType vertex_type = keep_block[i]->vertex_type;
-
-        if(vertex_type == ResidualBlockInfo::VERTEX_SE3_TCW) {
-            // delT = Tc_c0 = Tcw * Tc0w.inv();
-            Eigen::Map<const Sophus::SE3d> Tcw(parameters_raw[i]);
-            Eigen::Map<const Sophus::SE3d> Tc0w(keep_block[i]->data);
-            Eigen::Map<Sophus::SE3d> delT(dx.data() + idx);
-            delT = Tcw * Tc0w.inverse();
-        }
-        else if(vertex_type == ResidualBlockInfo::VERTEX_SE3_TWB) {
-            // delT = Tb0_b = Twb0.inv() * Twb;
-            Eigen::Map<const Sophus::SE3d> Twb(parameters_raw[i]);
-            Eigen::Map<const Sophus::SE3d> Twb0(keep_block[i]->data);
-            Eigen::Map<Sophus::SE3d> delT(dx.data() + idx);
-            delT = Twb0.inverse() * Twb;
-        }
-        else {
-            Eigen::Map<const Eigen::VectorXd> x(parameters_raw[i], size);
-            Eigen::Map<const Eigen::VectorXd> x0(keep_block[i]->data, size);
-            dx.segment(idx, size) = x - x0;
-        }
+        keep_block[i]->linearOMinus(parameters_raw[i], keep_block[i]->data.get(), dx.data() + idx);
     }
 
     Eigen::Map<Eigen::VectorXd> residuals(residual_raw, n);
@@ -101,13 +229,8 @@ bool MarginalizationFactor::Evaluate(double const *const *parameters_raw,
     if(jacobian_raw) {
         for(int i = 0; i < static_cast<int>(marginalization_info->keep_block.size()); ++i) {
             int size = marginalization_info->keep_block[i]->size;
+            int local_size = marginalization_info->keep_block[i]->local_size;
             int idx = marginalization_info->keep_block[i]->idx - m;
-            ResidualBlockInfo::VertexType vertex_type = keep_block[i]->vertex_type;
-            int local_size = size;
-            if(vertex_type == ResidualBlockInfo::VERTEX_SE3_TCW ||
-                    vertex_type == ResidualBlockInfo::VERTEX_SE3_TWB)
-                local_size = Sophus::SE3d::DoF;
-
             Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic,
                     Eigen::RowMajor>> jacobian(jacobian_raw[i], n, size);
             jacobian.setZero();
--- modules/ceres/marginalization_factor.h
+++ modules/ceres/marginalization_factor.h
@@ -1,64 +1,71 @@
 #pragma once
 // this code is reference from vins marginalization
+#include <sophus/se3.hpp>
 #include <ceres/ceres.h>
 #include "basic_datatype/util_datatype.h"
 
+class ParameterBlockInfo {
+public:
+    ParameterBlockInfo(int size_, int local_size_)
+        : size(size_), local_size(local_size_), idx(-1), data(nullptr) {}
+    ParameterBlockInfo(int size_)
+        : ParameterBlockInfo(size_, size_) {}
+    virtual void linearOMinus(const double* x, const double* x0, double* delx) const;
+
+    const int size;
+    const int local_size;
+    int idx;
+    std::unique_ptr<double[]> data;
+};
+SMART_PTR(ParameterBlockInfo)
+
+class SE3BlockInfo : public ParameterBlockInfo {
+public:
+    SE3BlockInfo()
+        : ParameterBlockInfo(Sophus::SE3d::num_parameters, Sophus::SE3d::DoF) {}
+    void linearOMinus(const double* x, const double* x0, double* delx) const override;
+};
+SMART_PTR(SE3BlockInfo)
+
 class ResidualBlockInfo {
 public:
-
-    enum VertexType {
-        VERTEX_LINEAR,
-        VERTEX_SE3_TCW,
-        VERTEX_SE3_TWB
-    };
-
     ResidualBlockInfo(std::shared_ptr<ceres::CostFunction> cost_function_,
                       std::shared_ptr<ceres::LossFunction> loss_function_,
                       const std::vector<double *>& parameter_blocks_,
-                      const std::vector<VertexType>& vertex_types_,
+                      const std::vector<ParameterBlockInfoPtr>& parameter_block_info_, // correspondence of parameter_blocks
                       const std::vector<int>& drop_set_)
         : cost_function(cost_function_), loss_function(loss_function_), parameter_blocks(parameter_blocks_),
-          vertex_types(vertex_types_), drop_set(drop_set_) {}
+          parameter_block_info(parameter_block_info_), drop_set(drop_set_) {}
     ~ResidualBlockInfo() {}
 
     void Evaluate();
 
     std::shared_ptr<ceres::CostFunction> cost_function;
     std::shared_ptr<ceres::LossFunction> loss_function;
-    std::vector<double*>    parameter_blocks; // double**
-    std::vector<VertexType> vertex_types;
+    std::vector<double*>    parameter_blocks;
+    std::vector<ParameterBlockInfoPtr> parameter_block_info;
     std::vector<int>        drop_set;
     std::vector<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> jacobians;
     Eigen::VectorXd residuals;
 };
-
 SMART_PTR(ResidualBlockInfo)
 
 class MarginalizationInfo {
 public:
-    struct ParameterBlock {
-        ParameterBlock(ResidualBlockInfo::VertexType vertex_type_, int size_, int idx_, double* data_)
-            : vertex_type(vertex_type_), size(size_), idx(idx_), data(data_) {}
-        ResidualBlockInfo::VertexType vertex_type;
-        int size;
-        int idx;
-        double* data;
-    };
-    SMART_PTR(ParameterBlock)
-
     void AddResidualBlockInfo(ResidualBlockInfoPtr residual_block_info);
+    void PreMarginalize();
+    void Marginalize();
 
     std::vector<ResidualBlockInfoPtr> factors;
     int m, n;
     int sum_block_size;
-    std::map<double*, ParameterBlockPtr> parameter_block;
-    std::vector<ParameterBlockPtr> keep_block;
+    std::map<double*, ParameterBlockInfoPtr> parameter_block;
+    std::vector<ParameterBlockInfoPtr> keep_block;
 
     Eigen::MatrixXd linearized_jacobian;
     Eigen::VectorXd linearized_residuals;
     const double eps = 1e-8;
 };
-
 SMART_PTR(MarginalizationInfo)
 
 // prior
