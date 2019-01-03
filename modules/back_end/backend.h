e97f2e8539757b81e05f54a91ec85c2f0070abfe
Yi-Ren <yirenliu0909@gmail.com>
2019-01-04 00:38:50 +0800

    add backend pipeline flow.

--- modules/back_end/backend.h
+++ modules/back_end/backend.h
@@ -3,16 +3,33 @@
 #include "basic_datatype/sliding_window.h"
 #include "camera_model/simple_stereo_camera.h"
 #include <thread>
+#include <condition_variable>
 
 class BackEnd {
 public:
+    enum State {
+        NEED_INIT,
+        CV_ONLY,
+        TIGHTLY_COUPLE
+    };
+
     BackEnd(SimpleStereoCamPtr camera_, SlidingWindowPtr sliding_window_);
+    ~BackEnd();
+
     void Process();
+    void AddKeyFrame(FramePtr keyframe);
+
+    State state;
 
     SimpleStereoCamPtr camera;
     SlidingWindowPtr sliding_window;
 
     // buffer
+    std::vector<FramePtr> v_kf_buffer;
+    std::mutex m_kf_buffer;
+    std::condition_variable cv_kf_buffer;
+private:
+    void ProcessKeyFrame(FramePtr keyframe);
 };
 
 SMART_PTR(BackEnd)
