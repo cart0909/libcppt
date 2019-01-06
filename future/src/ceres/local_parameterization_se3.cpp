efd305221c0f05af1e28c971f6d8fb7ff76f08eb
Yi-Ren <yirenliu0909@gmail.com>
2019-01-06 23:49:20 +0800

    local_parameterization_se3 decouple the translation and rotation and the SE3 data is first translation second rotation not same as Sophus::SE3d

---
+++ future/src/ceres/local_parameterization_se3.cpp
@@ -0,0 +1,40 @@
+#include "local_parameterization_se3.h"
+#include <sophus/se3.hpp>
+
+LocalParameterizationSE3::LocalParameterizationSE3() {}
+LocalParameterizationSE3::~LocalParameterizationSE3() {}
+
+// Generalization of the addition operation,
+//
+//   x_plus_delta = Plus(x, delta)
+//
+// with the condition that Plus(x, 0) = x.
+bool LocalParameterizationSE3::Plus(const double* x, const double* delta, double* x_plus_delta) const {
+    Eigen::Map<const Eigen::Vector3d> p(x), dp(delta), omega(delta + 3);
+    Eigen::Map<const Sophus::SO3d> q(x + 3);
+    Eigen::Map<Eigen::Vector3d> p_(x_plus_delta);
+    Eigen::Map<Sophus::SO3d> q_(x_plus_delta + 3);
+    Sophus::SO3d dq = Sophus::SO3d::exp(omega);
+    p_ = p + dp;
+    q_ = q * dq;
+    return true;
+}
+
+// The jacobian of Plus(x, delta) w.r.t delta at delta = 0.
+//
+// jacobian is a row-major GlobalSize() x LocalSize() matrix.
+bool LocalParameterizationSE3::ComputeJacobian(const double* x, double* jacobian) const {
+    Eigen::Map<Eigen::Matrix<double, 7, 6, Eigen::RowMajor>> J(jacobian);
+    J.setIdentity();
+    return true;
+}
+
+// Size of x.
+int LocalParameterizationSE3::GlobalSize() const {
+    return Sophus::SE3d::num_parameters;
+}
+
+// Size of delta.
+int LocalParameterizationSE3::LocalSize() const {
+    return Sophus::SE3d::DoF;
+}
