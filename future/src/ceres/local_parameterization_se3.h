dfd75b039aeb9a27247aab1613eda971dace00ef
Yi-Ren <yirenliu0909@gmail.com>
2019-01-06 23:49:20 +0800

    local_parameterization_se3 decouple the translation and rotation and the SE3 data is first translation second rotation not same as Sophus::SE3d

---
+++ future/src/ceres/local_parameterization_se3.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <ceres/local_parameterization.h>
+
+class LocalParameterizationSE3 : public ceres::LocalParameterization {
+public:
+    LocalParameterizationSE3();
+    virtual ~LocalParameterizationSE3();
+
+    // Generalization of the addition operation,
+    //
+    //   x_plus_delta = Plus(x, delta)
+    //
+    // with the condition that Plus(x, 0) = x.
+    virtual bool Plus(const double* x,
+                      const double* delta,
+                      double* x_plus_delta) const;
+
+    // The jacobian of Plus(x, delta) w.r.t delta at delta = 0.
+    //
+    // jacobian is a row-major GlobalSize() x LocalSize() matrix.
+    virtual bool ComputeJacobian(const double* x, double* jacobian) const;
+
+    // Size of x.
+    virtual int GlobalSize() const;
+
+    // Size of delta.
+    virtual int LocalSize() const;
+};
