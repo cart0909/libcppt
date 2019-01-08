1e1de53cff82a401fe09c83f0442056b2fe72ca6
Yi-Ren <yirenliu0909@gmail.com>
2019-01-09 01:01:43 +0800

    swap LocalParameterizationSE3::Plus trans and rotation

--- future/src/ceres/local_parameterization_se3.cpp
+++ future/src/ceres/local_parameterization_se3.cpp
@@ -10,10 +10,10 @@ LocalParameterizationSE3::~LocalParameterizationSE3() {}
 //
 // with the condition that Plus(x, 0) = x.
 bool LocalParameterizationSE3::Plus(const double* x, const double* delta, double* x_plus_delta) const {
-    Eigen::Map<const Eigen::Vector3d> p(x), dp(delta), omega(delta + 3);
-    Eigen::Map<const Sophus::SO3d> q(x + 3);
-    Eigen::Map<Eigen::Vector3d> p_(x_plus_delta);
-    Eigen::Map<Sophus::SO3d> q_(x_plus_delta + 3);
+    Eigen::Map<const Eigen::Vector3d> p(x + 4), dp(delta), omega(delta + 3);
+    Eigen::Map<const Sophus::SO3d> q(x);
+    Eigen::Map<Eigen::Vector3d> p_(x_plus_delta + 4);
+    Eigen::Map<Sophus::SO3d> q_(x_plus_delta);
     Sophus::SO3d dq = Sophus::SO3d::exp(omega);
     p_ = p + dp;
     q_ = q * dq;
