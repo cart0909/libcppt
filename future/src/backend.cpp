3453fcdeb3a24d36e12ba5408682bc3aa4e87402
carl_liu <yirenliu0909@gmail.com>
2019-01-10 18:09:00 +0800

    converter api introduce the simple way convert the data between feature_tacker/stereo_matcher frame to backend frame.

--- future/src/backend.cpp
+++ future/src/backend.cpp
@@ -3,8 +3,15 @@
 BackEnd::BackEnd(double focal_length_,
                  double gyr_n, double acc_n,
                  double gyr_w, double acc_w,
-                 const Eigen::Vector3d& p_rl_, const Eigen::Vector3d& q_rl_,
-                 const Sophus::SO3d& q_bc_, const Sophus::SO3d& p_bc_) {}
+                 const Eigen::Vector3d& p_rl_, const Eigen::Vector3d& p_bc_,
+                 const Sophus::SO3d& q_rl_, const Sophus::SO3d& q_bc_)
+    : focal_length(focal_length_), p_rl(p_rl_), p_bc(p_bc_), q_rl(q_rl_), q_bc(q_bc_)
+{
+    gyr_noise_cov = gyr_n * gyr_n * Eigen::Matrix3d::Identity();
+    acc_noise_cov = acc_n * acc_n * Eigen::Matrix3d::Identity();
+    gyr_bias_cov = gyr_w * gyr_w * Eigen::Matrix3d::Identity();
+    acc_bias_cov = acc_w * acc_w * Eigen::Matrix3d::Identity();
+}
 
 BackEnd::~BackEnd() {}
 
---
+++ future/src/converter.cpp
@@ -0,0 +1,28 @@
+#include "converter.h"
+
+BackEnd::FramePtr Converter::Convert(FeatureTracker::FramePtr feat_frame, CameraPtr cam_master,
+                                     StereoMatcher::FramePtr stereo_frame, CameraPtr cam_slave,
+                                     const Eigen::VecVector3d& v_gyr, const Eigen::VecVector3d& v_acc)
+{
+    BackEnd::FramePtr backend_frame(new BackEnd::Frame);
+    backend_frame->timestamp = feat_frame->timestamp;
+    backend_frame->pt_id = feat_frame->pt_id;
+    for(int i = 0, n = feat_frame->pt.size(); i < n; ++i) {
+        Eigen::Vector3d Pl, Pr;
+        backend_frame->pt.emplace_back(feat_frame->pt[i].x, feat_frame->pt[i].y);
+        Pl = cam_master->BackProject(backend_frame->pt[i]);
+        backend_frame->pt_normal_plane.emplace_back(Pl);
+        backend_frame->pt_r.emplace_back(stereo_frame->pt_r[i].x, stereo_frame->pt_r[i].y);
+        if(stereo_frame->pt_r[i].x == -1) {
+            backend_frame->pt_r_normal_plane.emplace_back(-1, -1, -1);
+        }
+        else {
+            Pr = cam_slave->BackProject(backend_frame->pt_r[i]);
+            backend_frame->pt_r_normal_plane.emplace_back(Pr);
+        }
+    }
+
+    backend_frame->v_gyr = v_gyr;
+    backend_frame->v_acc = v_acc;
+    return backend_frame;
+}
