cc4f4235f4f0f86a03e5866fcb9293826b8966b3
carl_liu <yirenliu0909@gmail.com>
2019-01-11 18:07:04 +0800

    prepare backend feature help maintain vio graph.

--- future/src/backend.cpp
+++ future/src/backend.cpp
@@ -1,16 +1,22 @@
 #include "backend.h"
+#include <glog/logging.h>
 
 BackEnd::BackEnd(double focal_length_,
                  double gyr_n, double acc_n,
                  double gyr_w, double acc_w,
                  const Eigen::Vector3d& p_rl_, const Eigen::Vector3d& p_bc_,
-                 const Sophus::SO3d& q_rl_, const Sophus::SO3d& q_bc_)
-    : focal_length(focal_length_), p_rl(p_rl_), p_bc(p_bc_), q_rl(q_rl_), q_bc(q_bc_)
+                 const Sophus::SO3d& q_rl_, const Sophus::SO3d& q_bc_,
+                 uint window_size_)
+    : focal_length(focal_length_), p_rl(p_rl_), p_bc(p_bc_), q_rl(q_rl_), q_bc(q_bc_), window_size(window_size_),
+      frame_count(0), next_frame_id(0), state(NEED_INIT)
 {
     gyr_noise_cov = gyr_n * gyr_n * Eigen::Matrix3d::Identity();
     acc_noise_cov = acc_n * acc_n * Eigen::Matrix3d::Identity();
     gyr_bias_cov = gyr_w * gyr_w * Eigen::Matrix3d::Identity();
     acc_bias_cov = acc_w * acc_w * Eigen::Matrix3d::Identity();
+
+    v_frames.resize(window_size_ + 1); // 10 old kf + 1 new kf
+    thread_ = std::thread(&BackEnd::Process, this);
 }
 
 BackEnd::~BackEnd() {}
@@ -18,6 +24,7 @@ BackEnd::~BackEnd() {}
 void BackEnd::PushFrame(FramePtr frame) {
     m_buffer.lock();
     frame_buffer.emplace_back(frame);
+    frame->id = next_frame_id++;
     m_buffer.unlock();
     cv_buffer.notify_one();
 }
@@ -31,5 +38,69 @@ void BackEnd::Process() {
             frame_buffer.clear();
             return !measurements.empty();
         });
+
+        for(auto& frame : measurements) {
+            ProcessFrame(frame);
+        }
+    }
+}
+
+void BackEnd::ProcessFrame(FramePtr frame) {
+    // test graph
+    for(int i = 0; i < frame->pt_id.size(); ++i) {
+        auto it = m_features.find(frame->pt_id[i]);
+        Feature* feat = nullptr;
+        if(it == m_features.end()) {
+            auto result = m_features.emplace(std::make_pair(frame->pt_id[i], Feature(frame->pt_id[i], frame_count)));
+            if(!result.second)
+                throw std::runtime_error("m_features insert fail?");
+            feat = &result.first->second;
+        }
+        else {
+            feat = &it->second;
+        }
+
+        feat->pt_n_per_frame.emplace_back(frame->pt_normal_plane[i]);
+        feat->pt_r_n_per_frame.emplace_back(frame->pt_r_normal_plane[i]);
+        if(frame->pt_r_normal_plane[i](0) == -1)
+            feat->num_meas += 1;
+        else
+            feat->num_meas += 2;
+    }
+
+    v_frames[frame_count] = frame;
+
+    if(frame_count >= window_size) {
+        // margin out old
+        for(auto it = m_features.begin(); it != m_features.end();) {
+            auto& feat = it->second;
+            if(feat.sliding_window_id == 0) {
+                // change parent
+                if(feat.pt_r_n_per_frame[0](0) == -1)
+                    feat.num_meas -= 1;
+                else
+                    feat.num_meas -= 2;
+
+                feat.pt_n_per_frame.pop_front();
+                feat.pt_r_n_per_frame.pop_front();
+
+                if(feat.num_meas < 2) {
+                    if(feat.pt_n_per_frame.empty() && feat.pt_r_n_per_frame.empty()) {
+                        it = m_features.erase(it);
+                    }
+                    else {
+                        throw std::runtime_error("feature num meas count wrong!?");
+                    }
+                }
+            }
+            else {
+                // TODO
+            }
+        }
+
+        v_frames.pop_front();
+    }
+    else {
+        ++frame_count;
     }
 }
--- future/src/backend.h
+++ future/src/backend.h
@@ -6,12 +6,20 @@
 
 class BackEnd {
 public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    enum State {
+        NEED_INIT,
+        CV_ONLY,
+        TIGHTLY
+    };
+
     enum MarginType {
-        MARGIN_OUT_NEW,
-        MARGIN_OUT_OLD
+        MARGIN_SECOND_NEW,
+        MARGIN_OLD
     };
 
     struct Frame {
+        EIGEN_MAKE_ALIGNED_OPERATOR_NEW
         uint64_t id;
         double timestamp;
         std::vector<uint64_t>  pt_id;
@@ -26,21 +34,49 @@ public:
         Eigen::Vector3d bg;
 
         Eigen::VecVector3d v_gyr, v_acc;
+        std::vector<double> v_imu_timestamp;
         ImuPreintegrationPtr imu_preintegration;
     };
     SMART_PTR(Frame)
+
+    struct Feature {
+        EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+        enum MapPointState {
+            NEED_INIT,
+            SUCCESS,
+            FAIL
+        };
+
+        Feature(uint64_t feat_id_, uint sliding_window_id_)
+            : feat_id(feat_id_), sliding_window_id(sliding_window_id_),
+              num_meas(0), mappoint_state(NEED_INIT), inv_depth(-1.0f) {}
+
+        uint64_t feat_id;
+        uint sliding_window_id;
+
+        uint num_meas; // num_mono * 1 + num_stereo * 2
+        Eigen::DeqVector3d pt_n_per_frame;
+        Eigen::DeqVector3d pt_r_n_per_frame;
+
+        MapPointState mappoint_state;
+        double inv_depth;
+    };
+    SMART_PTR(Feature)
 
     BackEnd(double focal_length_,
             double gyr_n, double acc_n,
             double gyr_w, double acc_w,
             const Eigen::Vector3d& p_rl_, const Eigen::Vector3d& p_bc_,
-            const Sophus::SO3d& q_rl_, const Sophus::SO3d& q_bc_);
+            const Sophus::SO3d& q_rl_, const Sophus::SO3d& q_bc_,
+            uint window_size_ = 10);
     ~BackEnd();
 
     void PushFrame(FramePtr frame);
-    void Process();
 
 private:
+    void Process();
+    void ProcessFrame(FramePtr frame);
     std::thread thread_;
     std::mutex  m_buffer;
     std::condition_variable cv_buffer;
@@ -54,5 +90,13 @@ private:
     Eigen::Matrix3d acc_noise_cov;
     Eigen::Matrix3d gyr_bias_cov;
     Eigen::Matrix3d acc_bias_cov;
+
+    std::map<uint64_t, Feature> m_features;
+    uint window_size;
+    uint frame_count;
+    std::deque<FramePtr> v_frames;
+
+    uint64_t next_frame_id;
+    State state;
 };
 SMART_PTR(BackEnd)
