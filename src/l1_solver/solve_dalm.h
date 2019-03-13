a949b35a787f9ac459062a57dd74ea3d9816ebbe
carl_liu <yirenliu0909@gmail.com>
2019-03-13 11:42:06 +0800

    Revert "implement l1_solver::dalm"
    This reverts commit 6394e380a5f7e2ba8a6c2312e642b6816cde2bcb.

--- src/l1_solver/solve_dalm.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <Eigen/Dense>
-// reference : https://people.eecs.berkeley.edu/~yang/software/l1benchmark/
-void SolveDALMFast(const Eigen::MatrixXd& A, const Eigen::VectorXd& b, double lambda,
-                   int max_iter, double tol, Eigen::VectorXd& x);
